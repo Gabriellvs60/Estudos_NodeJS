b7d63e7bb2a8056db457e925be3a2365de087d75
Gabriel Coelho <gabriellvs60@gmail.com>
2019-07-22 10:17:47 -0300

    Aula08-Parâmetros
    Conhecendo na aula 8, o uso de parâmetros no Node Js

--- Aula06-Express Js/index.js
+++ Aula06-Express Js/index.js
@@ -17,6 +17,17 @@ app.get("/blog", function(req, res){
     res.send("Bem Vindo ao meu blog!");
 });
 
+//Aula 08 parametros
+app.get('/ola/:cargo/:nome/:cor', function(req, res){
+    res.send("<h1>Ola " + req.params.nome+ "</h1>"  
+    + "<h2>Seu cargo e " + req.params.cargo + "</h2>"
+    + "<h3>Sua cor escolhida e " + req.params.cor + "</h3>"
+    );
+   
+});
+//o req é responsavel por receber dados de uma requisição
+//http://localhost:8081/ola/prof/adao/red
+
 app.listen(8081, function(){
     console.log("Servidor Rodando na url")
 });
