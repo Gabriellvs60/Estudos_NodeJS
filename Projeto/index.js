8395ad8fa3990c4a65dcd2bd3d04d4c39b0ef890
Gabriel Coelho <gabriellvs60@gmail.com>
2019-07-26 11:00:08 -0300

    Aula19-Handlebars
    Aprendendo um pouco sobre Handlebars.

--- Projeto/index.js
+++ Projeto/index.js
@@ -1,6 +1,20 @@
 const express = require("express");
 const app = express();
+const handlebars = require('express-handlebars')
 
+//Conexão com o BD Mysql
+const sequelize = new Sequelize ('test','root','password', {
+    host: "localhost",
+    dialect: 'mysql'
+})
+//Config
+    //Template Engine
+    app.engine('handlebars', handlebars({defaultLayout: 'main'}))
+    app.set('view engine', 'handlebars')
+    
+    app.get('/cad', function(req, res){
+        res.render('formulario')
+    })
 
 app.listen(8081, function(){
     console.log("Servidor Rodando na url")
